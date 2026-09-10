a9ed569ee7823ce20e782de97178f862a7258cd4
paulmshv <115788560+paulmshv@users.noreply.github.com>
2026-09-10 15:20:15 +0300

    Update vite.config.js
    Add info for deploy on Github Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/ServiceCatalogueAIHelper/', // Замените на имя вашего репозитория
   server: {
     host: "0.0.0.0",
     port: 3000,
